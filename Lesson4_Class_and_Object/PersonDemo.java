45b86fd9c13c3470cb8fc2debb14d7cfbfd46dd2
NguyenDuc2004 <145045445+NguyenDuc2004@users.noreply.github.com>
2025-12-15 14:55:30 +0700

    updateCode

--- Lesson4_Class_and_Object/PersonDemo.java
+++ Lesson4_Class_and_Object/PersonDemo.java
@@ -11,5 +11,25 @@ public class PersonDemo {
         System.out.println("Your Rectangle \n"+ rectangle.display());
         System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
         System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+
+        System.out.println("phuong trinh bac 2");
+        System.out.println("nhap 3 tham so");
+        double a,b,c;
+        System.out.println("a: ");
+        a = scanner.nextDouble();
+        System.out.println("b: ");
+        b = scanner.nextDouble();
+        System.out.println("c: ");
+        c = scanner.nextDouble();
+        QuadraticEquation pt1 = new QuadraticEquation(a,b,c);
+        if(pt1.getDiscriminant() > 0){
+            System.out.println("nghiem 1: " + pt1.getRoot1());
+            System.out.println("nghiem 2: " + pt1.getRoot2());
+        }else if(pt1.getDiscriminant() == 0){
+            System.out.println("nghiem kep: " + pt1.getRoot1());
+        }else{
+            System.out.println("The equation has no roots");
+        }
+
     }
 }
